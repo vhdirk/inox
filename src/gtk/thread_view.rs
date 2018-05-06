9ce5bad82607e539761d921caa5b0f306c09b329
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-05-06 20:10:06 +0200

    set webview properties

--- src/gtk/thread_view.rs
+++ src/gtk/thread_view.rs
@@ -9,6 +9,8 @@ use glib::translate::FromGlib;
 use gtk;
 use gtk::prelude::*;
 use webkit2gtk;
+use webkit2gtk_webextension;
+use webkit2gtk::SettingsExt;
 
 use relm_attributes::widget;
 
@@ -46,6 +48,33 @@ pub struct ThreadViewModel {
 #[widget]
 impl ::relm::Widget for ThreadView {
 
+    fn init_view(&mut self) {
+        let settings = webkit2gtk::WebViewExt::get_settings(&self.webview).unwrap();
+
+        // settings.set_enable_scripts(true);
+        // settings.set_enable_java_applet(false);
+        settings.set_enable_plugins(false);
+        settings.set_auto_load_images(true);
+        settings.set_enable_dns_prefetching(false);
+        settings.set_enable_fullscreen(false);
+        settings.set_enable_html5_database(false);
+        settings.set_enable_html5_local_storage(false);
+        //settings.set_enable_mediastream(false);
+        settings.set_enable_mediasource(false);
+        settings.set_enable_offline_web_application_cache(false);
+        settings.set_enable_page_cache(false);
+        settings.set_enable_private_browsing(true);
+        // settings.set_enable_running_of_insecure_content(false);
+        // settings.set_enable_display_of_insecure_content(false);
+        settings.set_enable_xss_auditor(true);
+        settings.set_media_playback_requires_user_gesture(true);
+        settings.set_enable_developer_extras(true); // TODO: should only enabled conditionally
+
+
+
+    }
+
+
     fn model() -> ThreadViewModel {
         ThreadViewModel {
 
@@ -57,8 +86,11 @@ impl ::relm::Widget for ThreadView {
     }
 
     view! {
-        gtk::Box{
+        gtk::ScrolledWindow{
+            #[name="webview"]
+            webkit2gtk::WebView{
 
+            }
         }
     }
 }
