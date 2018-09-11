d6f25d3fa56ad7ecd4a5f7c06652d433af6e353a
Dirk Van Haerenborgh <vhdirk@gmail.com>
2018-09-11 08:33:52 +0200

    wip: riker

--- enamel-gtk/src/constants.rs
+++ enamel-gtk/src/constants.rs
@@ -1,6 +1,6 @@
 
-pub const APPLICATION_ID: &'static str = "com.github.vhdirk.inox";
-pub const APPLICATION_NAME: &'static str = "Inox";
-pub const APPLICATION_CLASS: &'static str = "inox";
+pub const APPLICATION_ID: &'static str = "com.github.vhdirk.enamel";
+pub const APPLICATION_NAME: &'static str = "Enamel";
+pub const APPLICATION_CLASS: &'static str = "Enamel";
 
 pub const APPLICATION_ICON_NAME: &'static str = "mailbox";
--- enamel-gtk/src/main_window.rs
+++ enamel-gtk/src/main_window.rs
@@ -13,6 +13,7 @@ use rayon;
 // use hammond_data::{dbqueries, Source};
 
 use app::EnamelApp;
+use uibuilder::UI;
 use app::Action;
 use stacks::Content;
 // use utils::{itunes_to_rss, refresh};
@@ -23,29 +24,25 @@ use std::rc::Rc;
 // TODO: Factor out the hamburger menu
 // TODO: Make a proper state machine for the headerbar states
 pub struct MainWindow {
+    ui: UI,
     pub(crate) container: gtk::ApplicationWindow,
 }
 
-impl Default for MainWindow {
-    fn default() -> MainWindow {
-        let builder = gtk::Builder::new_from_resource("/com/github/vhdirk/Enamel/gtk/main_window.ui");
-
-        let container = builder.get_object("main_window").unwrap();
-
-        MainWindow{
-            container
-        }
-    }
-}
-
 impl MainWindow {
-    pub(crate) fn new(sender: &Sender<Action>) -> Rc<Self> {
-        let window = Rc::new(MainWindow::default());
-        Self::init(&window, &sender);
+    pub(crate) fn new(ui: UI, 
+                      application: gtk::Application) -> Rc<Self> {
+        let window = Rc::new(MainWindow{
+            ui: ui.clone(),
+            container: ui.builder
+                .get_object("main_window")
+                .expect("Couldn't find main_window in ui file.")
+        });
+        window.container.set_application(&application);
+        Self::init(&window);
         window
     }
 
-    pub(crate) fn init(this: &Rc<Self>, sender: &Sender<Action>) {
+    pub(crate) fn init(this: &Rc<Self>/*, sender: &Sender<Action>*/) {
         let weak = Rc::downgrade(this);
 
         //self.switch.set_stack(&content.get_stack());
