6adc30bf9ef19352f2202f88f73c770b8e8ec854
Dirk Van Haerenborgh <vhdirk@gmail.com>
2019-11-22 19:03:45 +0100

    switch to cursive for tui

--- enamel-tui/src/main.rs
+++ enamel-tui/src/main.rs
@@ -16,28 +16,18 @@ use dirs;
 use structopt::StructOpt;
 use structopt::clap::{App, Arg};
 
-use termion::event::Key;
-use termion::input::MouseTerminal;
-use termion::raw::IntoRawMode;
-use termion::screen::AlternateScreen;
-use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Direction, Layout, Rect};
-use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, Widget};
-use tui::Terminal;
+use cursive::Cursive;
+use cursive::views::{Dialog, TextView};
 
-mod util;
-
-use crate::util::event::{Event, Events};
 
 struct EnamelApp {
-    size: Rect,
+    // size: Rect,
 }
 
 impl Default for EnamelApp {
     fn default() -> EnamelApp {
         EnamelApp {
-            size: Rect::default(),
+            // size: Rect::default(),
         }
     }
 }
@@ -79,6 +69,23 @@ fn default_config_path() -> PathBuf{
 }
 
 
+// const COMMANDS = {
+//     "search": {},
+//     "envelope": {},
+//     "bufferlist": {},
+//     "taglist": {},
+//     "namedqueries": {},
+//     "thread": {},
+//     "global": {},
+// };
+
+
+// pub trait Command {
+//     fn apply() {}
+//     fn pre_hooks() {}
+//     fn post_hooks() {}
+// }
+
 fn main() -> Result<(), io::Error> {
     init();
 
@@ -90,7 +97,6 @@ fn main() -> Result<(), io::Error> {
         .create(default_config.to_str().unwrap()).unwrap();
 
     // let args = Args::from_args();
-
     let _args = App::new("Enamel")
         .version("0.0.1")
         .author("Dirk Van Haerenborgh <vhdirk@gmail.com>")
@@ -107,79 +113,18 @@ fn main() -> Result<(), io::Error> {
         .get_matches();
 
 
-    // Terminal initialization
-    let stdout = io::stdout().into_raw_mode()?;
-    let stdout = MouseTerminal::from(stdout);
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
+    let initial_command = "search tag:inbox AND NOT tag:killed";
 
-    // Create default app state
-    let mut app = EnamelApp::default();
+    // Creates the cursive root - required for every application.
+    let mut siv = Cursive::default();
 
-    // Setup event handlers
-    let events = Events::new();
+    // Creates a dialog with a single "Quit" button
+    siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
+                         .title("Cursive")
+                         .button("Quit", |s| s.quit()));
 
-    loop {
-        let size = terminal.size()?;
-        if app.size != size {
-            terminal.resize(size)?;
-            app.size = size;
-        }
+    // Starts the event loop.
+    siv.run();
 
-        terminal.draw(|mut f| {
-            // Wrapping block for a group
-            // Just draw the block and the group on the same area and build the group
-            // with at least a margin of 1
-            Block::default().borders(Borders::ALL).render(&mut f, size);
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(4)
-                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-                .split(app.size);
-            {
-                let chunks = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-                    .split(chunks[0]);
-                Block::default()
-                    .title("With background")
-                    .title_style(Style::default().fg(Color::Yellow))
-                    .style(Style::default().bg(Color::Green))
-                    .render(&mut f, chunks[0]);
-                Block::default()
-                    .title("Styled title")
-                    .title_style(
-                        Style::default()
-                            .fg(Color::White)
-                            .bg(Color::Red)
-                            .modifier(Modifier::Bold),
-                    ).render(&mut f, chunks[1]);
-            }
-            {
-                let chunks = Layout::default()
-                    .direction(Direction::Horizontal)
-                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
-                    .split(chunks[1]);
-                Block::default()
-                    .title("With borders")
-                    .borders(Borders::ALL)
-                    .render(&mut f, chunks[0]);
-                Block::default()
-                    .title("With styled borders")
-                    .border_style(Style::default().fg(Color::Cyan))
-                    .borders(Borders::LEFT | Borders::RIGHT)
-                    .render(&mut f, chunks[1]);
-            }
-        })?;
-
-        match events.next() {
-            Ok(Event::Input(key)) => if key == Key::Char('q') {
-                break;
-            },
-            _ => {}
-        }
-    }
     Ok(())
 }
