1b7dd0e6e01860c7af26ce0923339238b47f7658
Dirk Van Haerenborgh <vhdirk@gmail.com>
2021-12-13 09:52:26 +0100

    start working on jsonrpc core

---
+++ inox-core/src/protocol/message.rs
@@ -0,0 +1,9 @@
+use jsonrpc_core::Result;
+use jsonrpc_derive::rpc;
+
+#[rpc]
+pub trait Message {
+	/// Adds two numbers and returns a result
+	#[rpc(name="get")]
+	fn get(&self, id: String) -> Result<u64>;
+}
